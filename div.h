3a40447ba60d4ee3bdd60ed5dd22295ef3a72a6b
Dan <doates625@gmail.com>
2020-03-09 21:17:21 -0400

    Added and tested Cholesky right division

--- div.h
+++ div.h
@@ -10,7 +10,7 @@
 #include "bsub.h"
 
 /**
- * @brief Solve A*X = B in place with cholesky decomposition
+ * @brief Solve A*X = B in place with Cholesky decomposition
  * @param A Symmetric PD matrix
  * @param B Solution matrix B -> X
  */
@@ -21,4 +21,17 @@ void divl_chol(const MatrixExp<m, m>& A, Matrix<m, n>& B)
 	chol(L);			// A = L * L'
     bsubl(L, B);		// Solve L * Y = B
 	bsubu(trn(L), B);	// Solve L' * X = Y
+}
+
+/**
+ * @brief Solve X*A = B with Cholesky decomposition
+ * @param A Symmetric PD matrix
+ * @param B Target matrix
+ */
+template<uint8_t m, uint8_t n>
+Matrix<m, n> divr_chol(const MatrixExp<n, n>& A, const MatrixExp<m, n>& B)
+{
+	Matrix<n, m> trn_X = trn(B);
+	divl_chol(A, trn_X);	// X' = A \ B'
+	return trn(trn_X);
 }
